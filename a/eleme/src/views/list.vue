7e463e832699e952c7b94f2bb8eafde589fc01dc
T55330 <1225142877@qq.com>
2020-12-11 16:42:16 +0800

    1

--- a/eleme/src/views/list.vue
+++ a/eleme/src/views/list.vue
@@ -1,29 +1,29 @@
 <template>
     <div class="list">
         <header>
-            <h1>xxx</h1>
-            <router-link to="/home" class="goback-btn icon-chevron-left"></router-link>
-            <span class="total">a{{search_list.length}}b</span>
+            <h1>搜索结果</h1>
+            <router-link to='/home' class="goback-btn icon-chevron-left"></router-link>
+            <span class="total">共{{search_list.length}}条</span>
         </header>
-        <ul id="scrollMenu" class="good-list">
+        <ul id="scrolMenu" class="good-list">
             <li v-for='(item,index) in search_list' :key='index'>
-                <img :src="item.img" @click="show_detail(item)" alt="">
+                <img :src="item.img" @click="show_detail(item)"/>
                 <h4>{{item.name}}</h4>
                 <p>{{item.description}}</p>
-                <strong>${{item.price}}</strong>
-                <button @click='pointer(item)' :class='["point-btn",item.checked?"ac":""]' :disable='item.checked'>
-
+                <strong>￥{{item.price}}</strong>
+                <button @click="pointer(item)" 
+                :class="['pointer-btn',item.checked ? 'ac' : '']" :disabled="item.checked">
+                         <b>点餐</b><i>已点</i>
                 </button>
-
-            </li>
+                </li>
         </ul>
-        <div style="text-align=center" v-if='search_list.length==0'></div>
+                <div style='text-align=center' v-if='search_list.length=0'>没有搜到结果！</div>
     </div>
 </template>
 <script>
 export default ({
     name: 'list',
-    date(){
+    data(){
         return {
             shopping_cart:[],
             search_list:[],
@@ -31,12 +31,17 @@ export default ({
         }
     },
     created(){
+        // console.log(1);
         this.keyword = this.$store.state.keyword
         this.goods = this.$store.state.goods
+        console.log(this.goods);
         this.shopping_cartb = this.$store.shopping_cart
+        // console.log(1);
         for(let key in this.goods){
-            if(this.goods[key].name.includes(this.keywords)&& this.keyword != ''){
+            // console.log(1);
+            if(this.goods[key].name.includes(this.keyword)&& this.keyword != ''){
                 this.search_list.push(this.goods[key])
+                // console.log(1);
             }
         }
     }
